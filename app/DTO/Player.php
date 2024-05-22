611f8f0a5c64cbc4b3f3f9da86bdfe06e1903529
Arshavin <arsavinwallentyn@gmail.com>
2024-05-23 00:44:49 +0300

    Bugfix player name regex

--- app/DTO/Player.php
+++ app/DTO/Player.php
@@ -24,12 +24,12 @@ final class Player
     public function setName(string $name) {
         $this->name = $name;
         $this->shortName = preg_replace(
-            '/(\b.+?\b)([a-zA-ZăâșȘțî])[a-zA-ZăâșȘțî]+\b/u',
+            '/(\b.+?\b)([a-zA-ZăâșȘțȚî])[a-zA-ZăâșȘțȚî]+\b/u',
             '$1$2.',
             $name
         );
         $this->htmlShortName = preg_replace(
-            '/([a-zA-ZăâșȘțî]\.)/u',
+            '/([a-zA-ZăâșȘțȚî]\.)/u',
             '<small style="font-weight: 400; color: #444;"><small>$1</small></small>',
             $this->shortName
         );
