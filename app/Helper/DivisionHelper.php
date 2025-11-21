74f86e441e036656c4303cd3cae0e1ba49b04f33
Arshavin <arsavinwallentyn@gmail.com>
2025-11-21 23:39:56 +0200

    Insert MiniTour#29 new prizes

--- app/Helper/DivisionHelper.php
+++ app/Helper/DivisionHelper.php
@@ -7,9 +7,9 @@ final class DivisionHelper
     const DIVISION_COLORS = [
         'Yellow (e.g. Rookies)' => "#c2c21b",
         'Red (e.g. Beginners)' => "#e42844",
-        'Orange (e.g. Explorers)' => "#db9006",
         'Pink (e.g. Learners)' => "#db6a9d",
         'Blue (e.g. Novice)' => "#2b5dcb",
+        'Orange (e.g. Explorers)' => "#db9006",
         'Brown (e.g. Advancers)' => "#955443",
         'Black (e.g. Challengers)' => "#000000",
     ];
